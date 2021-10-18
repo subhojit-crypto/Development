7a725c9625a7cdb2a9041afcf5fa6945e9778560
subhojit-crypto <chatterjee.subhojit10@gmail.com>
2021-10-18 12:51:08 +0530

    Hello World Application React

---
+++ React/test-project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import helloWorld from './components/hello-world';
+
+function App() {
+  return (
+    <div className="App">
+      <helloWorld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/test-project/src/components/hello-world.js
@@ -0,0 +1,7 @@
+import Reacr from 'react'
+
+function helloWorld(){
+    return <h1>Hello World!!</h1>
+}
+
+export default helloWorld;
